95f5dc20744d3c90ed8220dc6d6ebedf317d5095
Tauqeer Ahmad <tauqeer.ahmad@powersoft19.com>
2019-08-01 20:36:52 +0500

    setup

---
+++ script.js
@@ -0,0 +1,3 @@
+var link = document.createElement('div');
+link.innerHTML = '<div style="position:fixed;bottom:15px;left:20px"><a style="font-family: \'Operator Mono\', sans-serif;font-size: 30px;font-weight: normal;color: #e7ad52;text-decoration: none;display: block;" target="_blank" href="https://git-scm.com/doc">git-scm.com/doc</a></div>';
+document.body.appendChild(link);
